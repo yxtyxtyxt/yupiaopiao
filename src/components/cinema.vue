cc62841907e28d99ddf1d87b0d06ad09d4399664
uouo <immengxingcehn@qq.com>
2017-06-12 19:59:21 +0800

    提交第二次

---
+++ src/components/cinema.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<div id="popover" class="mui-popover">
+			<ul class="mui-table-view">
+				<li class="mui-table-view-cell">
+					<a href="#">Item1</a>
+				</li>
+				<li class="mui-table-view-cell">
+					<a href="#">Item2</a>
+				</li>
+				<li class="mui-table-view-cell">
+					<a href="#">Item3</a>
+				</li>
+				<li class="mui-table-view-cell">
+					<a href="#">Item4</a>
+				</li>
+				<li class="mui-table-view-cell">
+					<a href="#">Item5</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+</template>
+
+<script>
+	export default {
+		name: 'cinema'
+	}
+	mui('.bottomPopover').popover('toggle');
+</script>
+
+<style>
+
+</style>
